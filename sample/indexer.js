e8e0562fee1d7e6afad6d5fdf524b68b36ca0d19
Garbin Huang <garbinh@gmail.com>
2014-10-19 14:24:12 +0800

    new feature: customer indexer supported

---
+++ sample/indexer.js
@@ -0,0 +1,10 @@
+var moment = require('moment');
+
+module.exports = {
+  index: function(item, options) {
+    return [
+      {index:{_index: 'listening_' + moment(item._source.cdate).format('YYYYMM'), _type:options.type || item._type, _id: item._id}},
+      item._source
+    ];
+  }
+};
