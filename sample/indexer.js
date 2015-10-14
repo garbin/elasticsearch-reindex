5a1d689cac944eab158247c27499eed4ac712927
Lumir Mrkva <lumir.mrkva@topmonks.com>
2015-10-14 02:25:43 +0200

    indexer reverted

--- sample/indexer.js
+++ sample/indexer.js
@@ -2,6 +2,12 @@ var moment = require('moment');
 
 module.exports = {
   // interval: months, weeks, days
-sharded: { field: 'date', start: "2015-01-08"}
+  sharded: { field: 'idate', interval:'5', start:'2014-09-01', end:'2014-10-01'},
 //  query: { match_all:{} },
+  index: function(item, options) {
+    return [
+      {index:{_index: 'listening_' + moment(item._source.cdate).format('YYYYMM'), _type:options.type || item._type, _id: item._id}},
+      item._source
+    ];
+  }
 };
