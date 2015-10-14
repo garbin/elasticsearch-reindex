a249c371cdb42f764b7ae923de3d9bc8166c3716
Lumir Mrkva <lumir.mrkva@topmonks.com>
2015-10-14 02:15:56 +0200

    improved concurrency
    - status is synchronized on master process
    - interval days is set according to concurrency
    - better from/to url parsing supporting elastic not on the root
    - some error logging improvements

--- sample/indexer.js
+++ sample/indexer.js
@@ -2,12 +2,6 @@ var moment = require('moment');
 
 module.exports = {
   // interval: months, weeks, days
-  sharded: { field: 'idate', interval:'5', start:'2014-09-01', end:'2014-10-01'},
+sharded: { field: 'date', start: "2015-01-08"}
 //  query: { match_all:{} },
-  index: function(item, options) {
-    return [
-      {index:{_index: 'listening_' + moment(item._source.cdate).format('YYYYMM'), _type:options.type || item._type, _id: item._id}},
-      item._source
-    ];
-  }
 };
